a7d78c49cee42d3acc58ae6580438d23af159473
Gonzalessandrini <alessandrinigonzalo3@gmail.com>
2024-02-11 21:21:10 -0300

    add reading list button responsive

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,25 +1,33 @@
+import React, { useState } from 'react';
 import Book from "../Book/Book";
 import { ReadingList } from "../ReadingList/ReadingList";
-import './BookList.css'; 
+import './BookList.css';
 import { useReadingList } from "../../Hooks/useReadingList";
 
-export function BookList ({books, removeBookFromList, addToBookList}) {
+export function BookList({ books, removeBookFromList, addToBookList }) {
     const { readingList } = useReadingList();
+    const [isReadingListVisible, setIsReadingListVisible] = useState(true);
+
+    const toggleReadingList = () => {
+        setIsReadingListVisible(!isReadingListVisible);
+    };
+
     return (
         <div className="books-list">
             <div className="books-section">
-              <h3>Libros disponibles ({books.length})</h3>
-              <ul className="books-container">
-                {books.map(book => (
-                    <li className='books__element' key={book.ISBN}>
-                      <Book data={book} removeBookFromList={removeBookFromList} />
-                    </li>
-                ))}
-              </ul>
+                <h3>Libros disponibles ({books.length})</h3>
+                <ul className="books-container">
+                    {books.map(book => (
+                        <li className='books__element' key={book.ISBN}>
+                            <Book data={book} removeBookFromList={removeBookFromList} />
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <div className="reading-list-section">
-              <h3>Lista de lectura ({readingList.length})</h3>
-              <ReadingList addToBookList={addToBookList} />
+            <button onClick={toggleReadingList} className="reading-list-toggle">Toggle Reading List</button>
+            <div className={`reading-list-section ${isReadingListVisible ? 'reading-list-active' : ''}`}>
+                <h3>Lista de lectura ({readingList.length})</h3>
+                <ReadingList addToBookList={addToBookList} />
             </div>
         </div>
     );
